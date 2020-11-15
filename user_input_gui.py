2e63442a5cf63dd8f6e2b2bc54a2a13b4c65a9ec
Tejas Kannampuzha <43386445+Kannampuzha@users.noreply.github.com>
2020-11-15 17:10:59 +0530

    Delete user_input_gui.py

--- user_input_gui.py
+++
@@ -1,5 +0,0 @@
-import tkinter as tk
-from tkinter import simpledialog
-ROOT = tk.Tk()
-ROOT.withdraw()
-num = int(simpledialog.askstring(title="Number of Objects",prompt="Enter the number of distict objects that will be trained"))
